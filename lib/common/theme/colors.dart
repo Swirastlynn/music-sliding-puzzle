a8c81da53484e6bb4dba0f99579184ee4432e2d7
Przemyslaw Lusnia <przemyslaw.lusnia@schibsted.com>
2022-02-05 21:59:07 +0100

    copy-paste official puzzle hackathon theme

---
+++ lib/common/theme/colors.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/widgets.dart';
+
+/// Defines the color palette for the puzzle UI.
+abstract class PuzzleColors {
+  static const Color primary1 = Color(0xFF041E3C);
+
+  static const Color primary2 = Color(0xFF042B59);
+
+  static const Color primary3 = Color(0xFF043875);
+
+  static const Color primary4 = Color(0xFF0553B1);
+
+  static const Color primary5 = Color(0xFF0468D7);
+
+  static const Color primary6 = Color(0xFF027DFD);
+
+  static const Color primary7 = Color(0xFF13B9FD);
+
+  static const Color primary8 = Color(0xFFB8EAFE);
+
+  static const Color primary9 = Color(0xFFE7F8FF);
+
+  static const Color primary0 = Color(0xFFFFFFFF);
+
+  static const Color black = Color(0xFF000000);
+
+  /// Black (opacity 20%)
+  static const Color black2 = Color(0x33000000);
+
+  static const Color grey1 = Color(0xFF4A4A4A);
+
+  static const Color grey2 = Color(0xFFA4A4A4);
+
+  static const Color grey3 = Color(0xFFE8EAED);
+
+  static const Color grey4 = Color(0xFFF1F3F4);
+
+  static const Color grey5 = Color(0xFFF8F9FA);
+
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// White (opacity 40%)
+  static const Color white2 = Color(0x66FFFFFF);
+
+  static const Color greenPrimary = Color(0xFF00504C);
+
+  static const Color green90 = Color(0xFF71F7EF);
+
+  static const Color green50 = Color(0xFF00857F);
+
+  static const Color bluePrimary = Color(0xFF003064);
+
+  static const Color blue90 = Color(0xFF75BFFF);
+
+  static const Color blue50 = Color(0xFF027DFD);
+
+  static const Color yellowPrimary = Color(0xFF676000);
+
+  static const Color yellow90 = Color(0xFFFFF475);
+
+  static const Color yellow50 = Color(0xFF9E9400);
+}
